795acef3001965628b905c9acc422c376081b497
gmurillofuentes <murillex1@hotmail.com>
2021-06-29 13:02:57 -0600

    Correccion valor default fecha

--- app/Models/EmpresaPruebas.php
+++ app/Models/EmpresaPruebas.php
@@ -12,4 +12,15 @@ class EmpresaPruebas extends Model
     protected $table = 'empresa_pruebas';
     
     protected $fillable = ['descripcion', 'fecha_creacion', 'con_emp', 'con_pue', 'fecha_inicio', 'fecha_limite', 'tiempo_limite', 'link_prueba', 'created_at', 'updated_at'];
+
+public static function boot()
+{
+    parent::boot();
+
+    static::saving(function ($model) {
+    	$dt = date("Y-m-d H:i:s");
+         
+        $model->fecha_creacion = $dt ;
+    });
+}    
 }
