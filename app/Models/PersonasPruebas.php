45391da4444f45fb0d629a912c1fbd2cb7858344
gmurillofuentes <murillex1@hotmail.com>
2021-06-29 13:18:15 -0600

    Correccion valor default fecha

--- app/Models/PersonasPruebas.php
+++ app/Models/PersonasPruebas.php
@@ -12,4 +12,16 @@ class PersonasPruebas extends Model
     protected $table = 'personas_pruebas';
     
     protected $fillable = ['con_emp', 'con_persona', 'hora_inicio', 'hora_finalizacion', 'minutos_utilizados',  'numsec_prueba', 'fecha_prueba', 'created_at', 'updated_at'];
+public static function boot()
+{
+    parent::boot();
+
+    static::saving(function ($model) {
+    	$dt = date("Y-m-d H:i:s");
+    	$hora = date("H:i:s");
+         
+        $model->fecha_prueba = $dt ;
+        $model->hora_inicio = $hora ;
+    });
+}        
 }
