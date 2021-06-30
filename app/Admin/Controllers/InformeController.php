87fde48e0fa0e745a153ad756d000b6ff14ffc01
Andres <laqs84@hotmail.com>
2021-06-29 22:09:04 -0600

    Cambios

--- app/Admin/Controllers/InformeController.php
+++ app/Admin/Controllers/InformeController.php
@@ -5,25 +5,19 @@ namespace App\Admin\Controllers;
 Use App\Models\Competencias;
 Use App\Models\CompetenciasTipos;
 Use App\Models\CompetenciasNiveles;
-
 Use App\Models\Preguntas;
 Use App\Models\PreguntasOpciones;
-
 Use App\Models\EmpresaPruebas;
 Use App\Models\EmpresaPruebasDetalle;
 Use App\Models\EmpresasCompetenciasNiveldominio;
 Use App\Models\Empresas;
 Use App\Models\EmpresasPuestosCompetencias;
-
 Use App\Models\Personas;
 Use App\Models\PersonasPruebas;
 Use App\Models\PersonasPruebasDetalle;
-
 Use App\Models\Puestos;
 Use App\Models\PuestosClasificacion;
-
 Use App\Models\CalificacionResultados;
-
 use Encore\Admin\Facades\Admin;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
@@ -35,44 +29,51 @@ use Exception;
 class InformeController extends Controller {
 
     public function index() {
-        
+
         $request = Admin::user();
 
-        if($request["con_emp"] !==0){
-         
-         $empresas = Empresas::where('con_emp', $request["con_emp"])->get();
-         $candidatos = PersonasPruebasDetalle::where('con_emp', $request["con_emp"])->get(); 
-        }else{
-            
-        
-        $candidatos = PersonasPruebasDetalle::all();
-        
-        $empresas = Empresas::all();
+        if ($request["con_emp"] !== 0) {
+
+            $empresas = Empresas::where('con_emp', $request["con_emp"])->get();
+            $candidatos = PersonasPruebasDetalle::where('con_emp', $request["con_emp"])->get();
+        } else {
+
+
+            $candidatos = PersonasPruebasDetalle::all();
+
+            $empresas = Empresas::all();
         }
+        $candidatoactual1 = 0;
         $candidatos_arr = array();
         foreach ($candidatos as $key => $value) {
+            $candidatoactual = $value["con_persona"];
+
             $empresas = Empresas::where('con_emp', $value['con_emp'])->get();
             $empresapruebas = EmpresaPruebas::where('numsec_prueba', $value['numsec_prueba'])->get();
             $idPuesto = $empresapruebas->pluck("con_pue")->first();
             $puesto = Puestos::where('con_pue', $idPuesto)->get();
-            $candidato = Personas::where('con_persona', $value["con_persona"])->get(); 
-            $competencia = Competencias::where('con_comp', $value['con_comp'])->get();
+            $candidato = Personas::where('con_persona', $value["con_persona"])->get();
+
+            $nombre = $candidato->pluck("nombres")->first() . " " . $candidato->pluck("apellido1")->first() . " " . $candidato->pluck("apellido2")->first();
+
             $candidatos_item = array(
-            "con_emp" => $empresas->pluck("descripcion")->first() ? $empresas->pluck("descripcion")->first() : "No hay dato",
-            "candidato" => $candidato->pluck("nombres")->first()." ".$candidato->pluck("apellido1")->first()." ".$candidato->pluck("apellido2")->first() ? $candidato->pluck("nombres")->first()." ".$candidato->pluck("apellido1")->first()." ".$candidato->pluck("apellido2")->first() : "No hay dato",
-            "puesto" => $puesto->pluck("descripcion")->first() ? $puesto->pluck("descripcion")->first() : "No hay dato",
-            "competencia" => $competencia->pluck("descripcion")->first() ? $competencia->pluck("descripcion")->first() : "No hay dato",
-            "acciones" => '<a style="font-size: 16px;color:green;" title="Descargar" href="#" onclick="detalle(this)" class="detalle-' . $value['con_prue_det'] . '"><i class="fa fa-download" aria-hidden="true"></i></a> | ');
-        array_push($candidatos_arr, $candidatos_item);
+                "con_emp" => $empresas->pluck("descripcion")->first() ? $empresas->pluck("descripcion")->first() : "No hay dato",
+                "candidato" => $nombre ? $nombre : "No hay dato",
+                "puesto" => $puesto->pluck("descripcion")->first() ? $puesto->pluck("descripcion")->first() : "No hay dato",
+                "fecha_creacion" => $value["created_at"] ? $value["created_at"] : "No hay dato",
+                "acciones" => '<a style="font-size: 16px;color:green;" title="Descargar" href="#" onclick="detalle(this)" class="detalle-' . $value['con_prue_det'] . '"><i class="fa fa-download" aria-hidden="true"></i></a> | <a style="font-size: 16px;color:green;" title="Ver en linea" href="#" onclick="ver(this)" class="ver-' . $value['con_prue_det'] . '"><i class="fa fa-chrome" aria-hidden="true"></i></a>');
+            $candidatoactual = $value["con_persona"];
+            if ($candidatoactual !== $candidatoactual1) {
+                $candidatoactual1 = $candidatoactual;
+                array_push($candidatos_arr, $candidatos_item);
+            }
         }
-        
-        return view('admin.candidatospruebas' , ['candidatos' => $candidatos_arr, '_user_'      => $this->getUserData()]);
-   }
 
 
-   
-   protected function getUserData()
-    {
+        return view('admin.candidatospruebas', ['candidatos' => $candidatos_arr, '_user_' => $this->getUserData()]);
+    }
+
+    protected function getUserData() {
         if (!$user = Admin::user()) {
             return [];
         }
@@ -83,34 +84,36 @@ class InformeController extends Controller {
     public function informe(Request $request) {
         try {
             $id = $request["id"];
-            
-            $prueba = PersonasPruebasDetalle::where('con_prue_det', $id)->get(); 
+
+            $prueba = PersonasPruebasDetalle::where('con_prue_det', $id)->get();
             $idEmpresa = $prueba->pluck("con_emp")->first();
             $idPersona = $prueba->pluck("con_persona")->first();
             $idTest = $prueba->pluck("con_test")->first();
-            $idComp= $prueba->pluck("con_comp")->first();
+            $idComp = $prueba->pluck("con_comp")->first();
             $idPreg = $prueba->pluck("con_preg")->first();
             $idOpci = $prueba->pluck("con_opci")->first();
             $numsec_prueba = $prueba->pluck("numsec_prueba")->first();
-            
+
             $empresas = Empresas::where('con_emp', $idEmpresa)->get();
             $empresapruebas = EmpresaPruebas::where('numsec_prueba', $numsec_prueba)->get();
-            
+
             $idPuesto = $empresapruebas->pluck("con_pue")->first();
             $puesto = Puestos::where('con_pue', $idPuesto)->get();
-            
-            
-            $candidato = Personas::where('con_persona', $idPersona)->get(); 
-            
+
+            $candidato = Personas::where('con_persona', $idPersona)->get();
+
             $competencia = Competencias::where('con_comp', $idComp)->get();
-            
+
             $califi = CalificacionResultados::where('con_comp', $idComp)->get();
-            
+
             $preguntas = Preguntas::where('con_comp', $idComp)->get();
-            
+
             $preguntasOpciones = PreguntasOpciones::where('con_comp', $idComp)->get();
-            
-            
+
+            $nombrecompleto = $candidato->pluck("nombres")->first() . " " . $candidato->pluck("apellido1")->first() . " " . $candidato->pluck("apellido2")->first();
+
+            $nombre = $candidato->pluck("nombres")->first();
+
             /* Set the PDF Engine Renderer Path */
             $domPdfPath = base_path('vendor/dompdf/dompdf');
             \PhpOffice\PhpWord\Settings::setPdfRendererPath($domPdfPath);
@@ -120,7 +123,28 @@ class InformeController extends Controller {
             $template = new\PhpOffice\PhpWord\TemplateProcessor(resource_path('INFORME.docx'));
 
             /* @ Replacing variables in doc file */
-            $template->setValue('nombrecompleto', 'Andres');
+            $template->setValue('nombrecompleto', $nombrecompleto);
+
+            $template->setValue('nombre', $nombre);
+
+            $section1 = $template->AddSection();
+            $section1->addText("RESULTADOS PERFIL COMPETENCIAL (PNEL)", array("size" => 12, "bold" => true, "align" => "center")); // Agregamos un titulo al documento con tamaño 22 y en negritas
+
+            $styleTable = array('borderSize' => 2, 'borderColor' => '888888', 'cellMargin' => 40); // el borde de la tabla de 6px, color de borde = #888 , ...
+            $styleFirstRow = array('borderBottomColor' => '0000FF', 'bgColor' => '66BBFF');
+            $template->addTableStyle('table1', $styleTable, $styleFirstRow);
+
+            $table1 = $section1->addTable("table1"); // creamos la tabla
+            $table1->addRow(); // agregamos una fila a la tabla
+            $table1->addCell()->addText("Nombre"); // agregamos la columna 1
+            $table1->addCell()->addText("Direccion"); // agregamos la columna 2
+            $table1->addCell()->addText("Email"); // agregamos la columna 3
+            $table1->addCell()->addText("Telefono"); // agregamos la columna 4
+
+            
+
+            //$template->setValue('nombrecompleto', 'Andres');
+            //$template->setValue('nombrecompleto', 'Andres');
 
             /* @ Save Temporary Word File With New Name */
             $saveDocPath = tempnam(sys_get_temp_dir(), 'PHPWord');
---
+++ resources/views/Admin/routes.php
@@ -0,0 +1,137 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+
+    $router->get('/', 'CompetenciasController@index')->name('home');
+    //Competencias
+    Route::get('competencias', 'CompetenciasController@index')->name('competencias.index');
+    Route::delete('competencias/delete/{id}', 'CompetenciasController@delete')->name('competencias.delete');
+    Route::post('competencias', 'CompetenciasController@store')->name('competencias.store');
+    
+    //Competencias Niveles
+    Route::get('competenciasniveles', 'CompetenciasNivelesController@index')->name('competenciasniveles.index');
+    Route::delete('competenciasniveles/delete/{id}', 'CompetenciasNivelesController@delete')->name('competenciasniveles.delete');
+    Route::post('competenciasniveles', 'CompetenciasNivelesController@store')->name('competenciasniveles.store');
+    
+    //Preguntas
+    Route::get('preguntas/{id}', 'PreguntasController@index')->name('preguntas.index');
+    Route::get('preguntas', 'PreguntasController@index')->name('preguntas.index');
+    Route::delete('preguntas/delete/{id}', 'PreguntasController@delete')->name('preguntas.delete');
+    Route::post('preguntas/{id}', 'PreguntasController@store')->name('preguntas.store');
+    Route::post('preguntas', 'PreguntasController@store')->name('preguntas.store');
+    
+    //Preguntas Opciones
+    Route::get('preguntas-opciones/{id}/{idpreg}', 'PreguntasOpcionesController@index')->name('preguntas_opciones.index');
+    Route::get('preguntas-opciones', 'PreguntasOpcionesController@index')->name('preguntas-opciones.index');
+    Route::delete('preguntas-opciones/delete/{id}', 'PreguntasOpcionesController@delete')->name('preguntas-opciones.delete');
+    Route::post('preguntas-opciones/{id}/{idpreg}', 'PreguntasOpcionesController@store')->name('preguntas-opciones.store');
+    Route::post('preguntas-opciones', 'PreguntasOpcionesController@store')->name('preguntas-opciones.store');
+
+    //$router->get('/', 'CalificacionResultadosController@index')->name('home');
+    //Calificacion resultados
+    Route::get('calificacion_resultados', 'CalificacionResultadosController@index')->name('calificacion_resultados.index');
+    Route::delete('calificacion_resultados/delete/{id}', 'CalificacionResultadosController@delete')->name('calificacion_resultados.delete');
+    Route::post('calificacion_resultados', 'CalificacionResultadosController@store')->name('calificacion_resultados.store');
+    Route::get('/getCompetencias/{id}', 'CalificacionResultadosController@getCompetencias');
+
+    
+    //Recomendaciones
+    Route::get('recomendaciones/{id}', 'RecomendacionesController@index')->name('recomendaciones.index');
+    Route::get('recomendaciones', 'RecomendacionesController@index')->name('recomendaciones.index');
+    Route::delete('recomendaciones/delete/{id}', 'RecomendacionesController@delete')->name('recomendaciones.delete');
+    Route::post('recomendaciones/{id}', 'RecomendacionesController@store')->name('recomendaciones.store');
+    Route::post('recomendaciones', 'RecomendacionesController@store')->name('recomendaciones.store');
+    
+    //Empresas
+    
+    Route::get('empresas', 'EmpresasController@index')->name('empresas.index');
+    
+    Route::delete('empresas/delete/{id}', 'EmpresasController@delete')->name('empresas.delete');
+    Route::post('empresas', 'EmpresasController@store')->name('empresas.store');
+    
+    //Empresas Puestos competencias
+    Route::get('empresaspuestoscompetencias', 'EmpresasPuestosCompetenciasController@index')->name('empresaspuestoscompetencias.index');
+    Route::get('empresaspuestoscompetencias/{id}', 'EmpresasPuestosCompetenciasController@index')->name('empresaspuestoscompetencias.index');
+    Route::delete('empresaspuestoscompetencias/delete/{id}', 'EmpresasPuestosCompetenciasController@delete')->name('empresaspuestoscompetencias.delete');
+    Route::post('empresaspuestoscompetencias/{id}', 'EmpresasPuestosCompetenciasController@store')->name('empresaspuestoscompetencias.store');
+    Route::get('empresaspuestoscompetencias/getDetalles/', 'EmpresasPuestosCompetenciasController@getDetalles');
+    
+     //Empresas Niveles
+    Route::get('empresasniveles', 'EmpresasNivelesController@index')->name('empresasniveles.index');
+    Route::delete('empresasniveles/delete/{id}', 'EmpresasNivelesController@delete')->name('empresasniveles.delete');
+    Route::post('empresasniveles', 'EmpresasNivelesController@store')->name('empresasniveles.store');
+
+    //Empresas pruebas
+    Route::get('empresa_pruebas', 'EmpresaPruebasController@index')->name('empresa_pruebas.index');
+    
+    Route::delete('empresa_pruebas/delete/{id}', 'EmpresaPruebasController@delete')->name('empresa_pruebas.delete');
+    Route::post('empresa_pruebas', 'EmpresaPruebasController@store')->name('empresa_pruebas.store');
+
+    //Empresas pruebas detalle
+    Route::get('empresa_pruebas_detalle/{id}', 'EmpresaPruebasDetalleController@index')->name('empresa_pruebas_detalle.index');
+    
+    Route::delete('empresa_pruebas_detalle/delete/{id}', 'EmpresaPruebasDetalleController@delete')->name('empresa_pruebas_detalle.delete');
+    Route::post('empresa_pruebas_detalle/{id}', 'EmpresaPruebasDetalleController@store')->name('empresa_pruebas_detalle.store');
+
+    //Empresas pruebas base
+    Route::get('empresas_pruebas_base', 'EmpresasPruebasBaseController@index')->name('empresas_pruebas_base.index');
+    
+    Route::delete('empresas_pruebas_base/delete/{id}', 'EmpresasPruebasBaseController@delete')->name('empresas_pruebas_base.delete');
+    Route::post('empresas_pruebas_base', 'EmpresasPruebasBaseController@store')->name('empresas_pruebas_base.store');
+
+    //Empresas pruebas base detalle
+    Route::get('emp_pru_base_detalle/{id}', 'EmpresasPruebasBaseDetalleController@index')->name('emp_pru_base_detalle.index');
+    
+    Route::delete('emp_pru_base_detalle/delete/{id}', 'EmpresasPruebasBaseDetalleController@delete')->name('emp_pru_base_detalle.delete');
+    Route::post('emp_pru_base_detalle/{id}', 'EmpresasPruebasBaseDetalleController@store')->name('emp_pru_base_detalle.store');
+
+    //Personas pruebas
+    Route::get('personas_pruebas/{id}/{per}', 'PersonasPruebasController@index')->name('personas_pruebas.index');
+    Route::get('personas_pruebas/test/{id}/{per}/{lastid}', 'PersonasPruebasController@test')->name('personas_pruebas.test');  
+    Route::get('personas_pruebas/test/{id}/{per}/getPreguntas/{con_test}', 'PersonasPruebasController@getPreguntas')->name('personas_pruebas.getPreguntas');
+    Route::get('personas_pruebas/test/{id}/{per}/getNextTest/{con_test}', 'PersonasPruebasController@getNextTest')->name('personas_pruebas.getNextTest');    
+    Route::get('personas_pruebas/test/{id}/{per}/updatePrueba/{con_test}', 'PersonasPruebasController@updatePrueba')->name('personas_pruebas.updatePrueba');        
+    Route::delete('personas_pruebas/delete/{id}', 'PersonasPruebasController@delete')->name('personas_pruebas.delete');
+    Route::post('personas_pruebas/{id}/{per}', 'PersonasPruebasController@store')->name('personas_pruebas.store');
+
+    
+     //Empresas tipos
+    Route::get('competenciastipos', 'CompetenciasTiposController@index')->name('competenciastipos.index');
+    Route::delete('competenciastipos/delete/{id}', 'CompetenciasTiposController@delete')->name('competenciastipos.delete');
+    Route::post('competenciastipos', 'CompetenciasTiposController@store')->name('competenciastipos.store');
+    
+    //Puestos
+    Route::get('puestos', 'PuestosController@index')->name('puestos.index');
+    Route::delete('puestos/delete/{id}', 'PuestosController@delete')->name('puestos.delete');
+    Route::post('puestos', 'PuestosController@store')->name('puestos.store');
+    
+    //Puestos Clasificaciones
+    Route::get('puestosclasificaciones', 'PuestosClasificacionController@index')->name('puestosclasificaciones.index');
+    Route::delete('puestosclasificaciones/delete/{id}', 'PuestosClasificacionController@delete')->name('puestosclasificaciones.delete');
+    Route::post('puestosclasificaciones', 'PuestosClasificacionController@store')->name('puestosclasificaciones.store');
+    
+    //Administradores
+    Route::get('administradores', 'AdministradoresControllers@index')->name('administradores.index');
+    Route::get('administradores/{id}', 'AdministradoresControllers@index')->name('administradores.index');
+    Route::delete('administradores/delete/{id}', 'AdministradoresControllers@delete')->name('administradores.delete');
+    Route::post('administradores', 'AdministradoresControllers@store')->name('administradores.store');
+    
+    
+    Route::get('informe', 'InformeController@index')->name('informe.index');
+    
+    
+    //Candidatos
+    Route::get('candidatos', 'CandidatosController@index')->name('candidatos.index');
+    Route::get('candidatos/{id}', 'CandidatosController@index')->name('candidatos.index');
+    Route::delete('candidatos/delete/{id}', 'CandidatosController@delete')->name('candidatos.delete');
+    Route::post('candidatos', 'CandidatosController@store')->name('candidatos.store');
+});
